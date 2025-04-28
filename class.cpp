c73dd09ababd0b0a990009239d6cb38f0476c39f
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-04-28 11:11:11 +0700

    commit 4

--- class.cpp
+++ class.cpp
@@ -9,4 +9,17 @@ class Mobil
 
 private:
 	int harga_mobil;
+
+	public:
+	void input_data(){
+		cout << "Masukkan nama mobil : ";
+		cin >> nama_mobil;
+		cout << "Masukkan tipe mobil : ";
+		cin >> tipe_mobil;
+		cout << "Masukkan harga mobil : ";
+		cin >> harga_mobil;
+	}
+	void output_data(){
+		cout << "harga mobil = " << harga_mobil <<endl;
+	}
 };
